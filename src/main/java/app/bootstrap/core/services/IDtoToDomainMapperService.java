d4e5adac505c012d58384ef36ff49815fd5da90d
Nicklas Körtge <nicklas.koertge1@ibm.com>
2025-04-21 11:39:13 +0200

    update mapper
    Signed-off-by: Nicklas Körtge <nicklas.koertge1@ibm.com>

--- src/main/java/app/bootstrap/core/services/IDtoToDomainMapperService.java
+++ src/main/java/app/bootstrap/core/services/IDtoToDomainMapperService.java
@@ -6,13 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface IDtoToDomainMapperService <DTO, DOMAIN> {
-    @Nonnull DOMAIN map(@Nonnull DTO dto) throws Exception;
+    @Nonnull DOMAIN mapToDomain(@Nonnull DTO dto) throws Exception;
+
+    @Nonnull DTO mapToDto(@Nonnull DOMAIN dto) throws Exception;
 
     @Nonnull
-    default List<DOMAIN> map(@Nonnull List<DTO> dtoList) throws Exception {
+    default List<DOMAIN> mapToDomain(@Nonnull List<DTO> dtoList) throws Exception {
         final List<DOMAIN> domainList = new ArrayList<>();
         for (DTO dto : dtoList) {
-            domainList.add(map(dto));
+            domainList.add(mapToDomain(dto));
+        }
+        return domainList;
+    }
+
+    @Nonnull
+    default List<DTO> mapToDto(@Nonnull List<DOMAIN> dtoList) throws Exception {
+        final List<DTO> domainList = new ArrayList<>();
+        for (DOMAIN dto : dtoList) {
+            domainList.add(mapToDto(dto));
         }
         return domainList;
     }
