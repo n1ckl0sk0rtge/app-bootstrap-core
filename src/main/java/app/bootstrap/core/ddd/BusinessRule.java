1d0400ac52609f9ea74119a688c193be8ef23625
Nicklas Körtge <nicklas.koertge1@ibm.com>
2025-06-08 16:54:35 +0200

    add business rule annotation
    Signed-off-by: Nicklas Körtge <nicklas.koertge1@ibm.com>

---
+++ src/main/java/app/bootstrap/core/ddd/BusinessRule.java
@@ -0,0 +1,12 @@
+package app.bootstrap.core.ddd;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD})
+public @interface BusinessRule {
+    String rule() default "";
+}
